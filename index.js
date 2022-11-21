cecacde3e3e50257d87fde045b743f596d294ab2
rose534 <wambuirose534@gmail.com>
2022-11-21 19:51:44 +0300

    dom-modifying-elements

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+document.querySelector("#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.textContent = "rose is the champion.";
+newHeader.setAttribute("id", "victory");
+
+
+
+
+
+
+
